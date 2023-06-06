cc44d7044b0244e526d60013799946ae082cdddc
cmandrell <40442817+cmandrell@users.noreply.github.com>
2023-06-05 19:12:36 -0500

    Partial fix file path
    Corrected path to match proposed
    
    Bugs: has exposure as 0.4, 4.0, etc. controlled by SharpCap. Work around not impossible
             won't shift to .png if already in .tif : This is a SharpCap bug, as adding wait time or PrepareToCapture() does not fix

---
+++ Partial_Castor_edits.py
@@ -0,0 +1,53 @@
+# Capture .ser files with single.png for "ser_time" seconds at 1000x1000 resolution
+#
+# Uses user settings for exposure and gain
+# Forces resolution, binning, colour space
+# Returns to users resolution
+#####################################################################################
+
+import time
+
+ser_time = 5 #.ser collection duration in seconds at users exposure
+ 
+str_date = time.strftime("%d%b%Y",time.localtime())
+path = r'C:\DEB\\'+str_date+r'\Partial' # path for main capture folder
+
+str_time = time.strftime("%H%M%S",time.gmtime())
+
+### Initial Setup ####################################################################
+SharpCap.SelectedCamera.LiveView = False
+SharpCap.Settings.UseSubFolders = False
+SharpCap.Settings.CaptureFolder = path
+SharpCap.TargetName = 'partial_'+str_time
+#This part can be handled in settings of SharpCap
+SharpCap.Settings.UseManualTemplates = True # still need this if handled in settings
+SharpCap.Settings.ManualSequenceTemplate = '{TargetName}_{Exposure}'
+SharpCap.Settings.ManualSingleFileTemplate = '{TargetName}_{Exposure}'
+#
+SharpCap.SelectedCamera = SharpCap.Cameras[0]
+
+#reset values for returning at end of collection
+reset_area = SharpCap.SelectedCamera.Controls.Resolution.Value
+
+#Force values
+SharpCap.SelectedCamera.Controls.Binning.Value = '1'
+SharpCap.SelectedCamera.Controls.OutputFormat.Automatic = False
+SharpCap.SelectedCamera.Controls.ColourSpace.Value = 'MONO16'
+SharpCap.SelectedCamera.Controls.Resolution.Value = '1000x1000'
+
+### Capture images ####################################################################
+SharpCap.SelectedCamera.Controls.OutputFormat.Value = 'PNG file (*.png)'
+SharpCap.SelectedCamera.CaptureSingleFrame()
+
+SharpCap.SelectedCamera.LiveView = True
+SharpCap.SelectedCamera.Controls.OutputFormat.Value = 'SER file (*.ser)'   
+SharpCap.SelectedCamera.PrepareToCapture()
+SharpCap.SelectedCamera.RunCapture()
+time.sleep(ser_time)
+SharpCap.SelectedCamera.StopCapture()  
+
+### Reset resolution ###################################################################
+SharpCap.SelectedCamera.Controls.Resolution.Value = reset_area
+SharpCap.Settings.UseManualTemplates = False 
+
+# END OF PROGRAM
